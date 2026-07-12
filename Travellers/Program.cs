49ca8250fb03739032100cd31c9345dc2bf3bec6
Nicholas Lemay <lemaynicholas@gmail.com>
2026-07-12 19:48:41 -0400

    Rename UsersServices to UsersModuleConfiguration
    Structural changes. Do not use service name to avoid confusion with DDD services

--- Travellers/Program.cs
+++ Travellers/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services
     .AddTravellersDatabase(builder.Configuration)
-    .AddUsers();
+    .AddUsersModule();
 
 var app = builder.Build();
 
--- Travellers/Users/UsersModuleConfiguration.cs
+++ Travellers/Users/UsersModuleConfiguration.cs
@@ -2,9 +2,9 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Travellers.Users;
 
-public static class UsersServices
+public static class UsersModuleConfiguration
 {
-    public static IServiceCollection AddUsers(this IServiceCollection services)
+    public static IServiceCollection AddUsersModule(this IServiceCollection services)
     {
         services.AddScoped<IUserRepository, UsersRepository>();
 
